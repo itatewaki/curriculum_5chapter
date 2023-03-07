1ccb9596f3ead148df261f44809940c1eadb819f
IsaoTatewaki <isao.tatewaki@gmail.com>
2023-03-07 11:56:35 +0900

    ログインの有無による表示の切り替え

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,7 +24,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     {{-- この章の後半で作成するCSSを読み込みます --}}
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
-  </head>
+ </head>
 
   <body>
     <div id="app">
@@ -45,6 +45,29 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
+              <!-- Authentication Links -->
+              @guest
+                {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login')}}</a></li>
+              @else
+                <li>
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                  </a>
+
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout')}}"
+                      onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                    </form>
+                  </div>
+                </li>
+              @endguest
             </ul>
           </div>
         </div>
