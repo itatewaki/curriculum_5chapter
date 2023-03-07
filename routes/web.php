f93b14db5c01cfa2af368318d9d5bb5206dca8c1
IsaoTatewaki <isao.tatewaki@gmail.com>
2023-03-07 11:11:11 +0900

    日本語化とブラック背景

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
