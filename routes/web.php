702cf73b88f772964488d892d8b17d48e661a0a9
IsaoTatewaki <isao.tatewaki@gmail.com>
2023-03-08 17:24:53 +0900

    ログインしていない場合のリダイレクト処理

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 Auth::routes();
 
