6943f13fbc3bf461dd9cafbf93f041049f864dca
IsaoTatewaki <isao.tatewaki@gmail.com>
2023-03-09 12:15:27 +0900

    ナビゲーションバーのユーザ名とログアウトボタンを設置

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -47,6 +47,32 @@
 
             <!-- Right side Of Navbar -->
             <ul class="navbar-nav ml-auto">
+              <!-- ナビゲーションバーにログインリンクとログアウトリンクを設置 -->
+              @guest
+                {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login')}}</a></li>
+              @else
+                {{-- ログインしていたらユーザ名とログアウトボタンを表示 --}}
+                <li class="nav-item dropdown">
+                  {{-- ログインユーザ名を取得して表示 --}}
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                  </a>
+                  
+                  {{-- ログアウトボタン --}}
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                          onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POSTT" style="display: none;">
+                      @csrf
+                    </form>
+                  </div>
+                </li>
+              @endguest
             </ul>
           </div>
         </div>
