1f1e6e103abe8f55bd8c977fbd8662e38886b6cb
IsaoTatewaki <isao.tatewaki@gmail.com>
2023-03-11 10:26:16 +0900

    NewsController.phpの編集

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでNews Modelが扱えるようになる
+use App\News;
+
 class NewsController extends Controller
 {
     // add アクションの追加
@@ -14,6 +17,29 @@ class NewsController extends Controller
 
     // create アクションの追加
     public function create(Request $request) {
+        // Validationを行う
+        $this->validate($request, News::$rules);
+
+        $news = new News;
+        $form = $request->all();
+
+        // フォームから画像が送信されてきたら、保存して、$news->image_pathに画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+
+        // フォームから送信されてきた _tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきた imageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $news->file($form);
+        $news->save();
+        
         // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
     }
