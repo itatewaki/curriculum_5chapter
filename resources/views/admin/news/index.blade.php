dfc393fbdcd02e5f930223f0743649289606e1df
IsaoTatewaki <isao.tatewaki@gmail.com>
2023-03-11 16:01:49 +0900

    index.blade.phpのバグ修正

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -14,12 +14,12 @@
       <div class="col-md-8">
         <form action="{{ action('Admin\NewsController@index') }}" method="get">
           <div class="form-group row">
-            <label class=col-md-2>タイトル</label>
+            <label class="col-md-2">タイトル</label>
             <div class="col-md-8">
               <input type="text" class="form-control" name="cond_title" value="{{ $cond_title }}">
             </div>
             <div class="col-md-2">
-              {{ csrf_field }}
+              {{ csrf_field() }}
               <input type="submit" class="btn btn-primary" value="検索">
             </div>
           </div>
