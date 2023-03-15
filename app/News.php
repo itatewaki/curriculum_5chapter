7aa7ef19e61fe4a140c4b0fcd402a002d0260ed2
IsaoTatewaki <isao.tatewaki@gmail.com>
2023-03-15 14:27:59 +0900

    Newsモデルへの関連付け

--- app/News.php
+++ app/News.php
@@ -6,11 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
+    // ブラックリスト形式による割り当て許可
     protected $guarded = array('id');
 
-    //
+    // Validationルールの定義
     public static $rules = array(
         'title' => 'required',
         'body' => 'required',
     );
+
+    // Newsモデルへの関連付け
+    public function histories()
+    {
+      return $this->hasMany('App\History');
+
+    }
 }
